ada0063e2bddf55ca6aa7ae61859afe713fcf43e
Diego Jimenez <diegourieljd@gmail.com>
2023-04-04 17:41:04 -0600

    constructors, getters and setters added

--- springboot-items-microservice/src/main/java/com/djimenez/items/models/Item.java
+++ springboot-items-microservice/src/main/java/com/djimenez/items/models/Item.java
@@ -1,5 +1,15 @@
 package com.djimenez.items.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 public class Item {
 
     private Product product;
