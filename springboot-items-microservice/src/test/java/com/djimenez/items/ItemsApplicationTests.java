f1190abb415fa5f1a5adcf81a9e48e74ea65041c
Diego Jimenez <diegourieljd@gmail.com>
2023-04-04 16:45:07 -0600

    spring boot items project created

---
+++ springboot-items-microservice/src/test/java/com/djimenez/items/ItemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djimenez.items;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
