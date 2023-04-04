ef33d4523f1ce9c3cf0bfa3f7de1671ee90d4e95
Diego Jimenez <diegourieljd@gmail.com>
2023-04-04 17:17:07 -0600

    test product data added

---
+++ springboot-products-microservice/src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO products (name, price, create_at) VALUES ('Panasonic', 800, NOW());
+INSERT INTO products (name, price, create_at) VALUES ('Sony', 800, NOW());
+INSERT INTO products (name, price, create_at) VALUES ('Apple', 1200, NOW());
+INSERT INTO products (name, price, create_at) VALUES ('Hewlett', 900, NOW());
+INSERT INTO products (name, price, create_at) VALUES ('Lenovo', 700, NOW());
+INSERT INTO products (name, price, create_at) VALUES ('Nike', 200, NOW());
+INSERT INTO products (name, price, create_at) VALUES ('Dell', 100, NOW());
+INSERT INTO products (name, price, create_at) VALUES ('Adidas', 300, NOW());
+INSERT INTO products (name, price, create_at) VALUES ('New Era', 400, NOW());
