ca520edf7386e8c7c6ef711d7bc3be789fb02dc7
Diego Jimenez <diegourieljd@gmail.com>
2023-04-04 17:01:18 -0600

    product entity added

---
+++ springboot-products-microservice/src/main/java/com/djimenez/products/models/entity/Product.java
@@ -0,0 +1,35 @@
+package com.djimenez.products.models.entity;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Table(name = "products")
+public class Product implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private Double price;
+
+
+    @Column(name = "create_at")
+    private LocalDate createAt;
+
+    private static final long serialVersionUID = -1714065901434624484L;
+
+}
