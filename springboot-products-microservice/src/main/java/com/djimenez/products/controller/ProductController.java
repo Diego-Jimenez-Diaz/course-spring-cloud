d3aaf33a3a2d46b8743658a67391a874891424d2
Diego Jimenez <diegourieljd@gmail.com>
2023-04-05 22:33:32 -0600

    port information added to the response

--- springboot-products-microservice/src/main/java/com/djimenez/products/controller/ProductController.java
+++ springboot-products-microservice/src/main/java/com/djimenez/products/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.djimenez.products.controller;
 import com.djimenez.products.models.entity.Product;
 import com.djimenez.products.models.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,21 +12,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+	
+	@Value("${server.port}")
+	private Integer port;
 
     @Autowired
     private ProductService productService;
 
     @GetMapping
     public ResponseEntity<List<Product>> getAllProducts() {
-        return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
+        return new ResponseEntity<>(productService.getAll().stream().map(product -> {
+        	//product.setPort(Integer.valueOf(environment.getProperty("local.server.port")));
+        	product.setPort(port);
+        	return product;
+        }).collect(Collectors.toList()) , HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Product> findProductById(@PathVariable Long id) {
+    	Product product = productService.findById(id); 
+    	//product.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+    	product.setPort(port);
         return new ResponseEntity<>(productService.findById(id), HttpStatus.OK);
     }
+    
 }
--- springboot-products-microservice/src/main/java/com/djimenez/products/models/entity/Product.java
+++ springboot-products-microservice/src/main/java/com/djimenez/products/models/entity/Product.java
@@ -8,6 +8,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
+
 import java.io.Serializable;
 import java.time.LocalDate;
 
@@ -29,6 +31,10 @@ public class Product implements Serializable {
 
     @Column(name = "create_at")
     private LocalDate createAt;
+    
+    //Esta anotación se usa para que este atributo no se tome como un campo para la BD
+    @Transient
+    private Integer port;
 
     private static final long serialVersionUID = -1714065901434624484L;
 
