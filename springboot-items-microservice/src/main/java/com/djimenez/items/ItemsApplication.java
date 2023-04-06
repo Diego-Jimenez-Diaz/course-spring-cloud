37748fe89ac647a809b5f5fef6f4ce85d18bca38
Diego Jimenez <diegourieljd@gmail.com>
2023-04-05 23:53:30 -0600

    eureka server implemented

--- springboot-items-microservice/src/main/java/com/djimenez/items/ItemsApplication.java
+++ springboot-items-microservice/src/main/java/com/djimenez/items/ItemsApplication.java
@@ -2,12 +2,12 @@ package com.djimenez.items;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableFeignClients
-@RibbonClient(name = "products-service")
 public class ItemsApplication {
 
 	public static void main(String[] args) {
