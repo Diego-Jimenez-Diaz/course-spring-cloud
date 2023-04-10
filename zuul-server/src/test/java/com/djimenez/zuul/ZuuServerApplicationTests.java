c3277a4b41032c20ab9b884c79afd1dba3b1d5f0
Diego Jimenez <diegourieljd@gmail.com>
2023-04-10 13:44:37 -0600

    zuul server created

---
+++ zuul-server/src/test/java/com/djimenez/zuul/ZuuServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djimenez.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuuServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
