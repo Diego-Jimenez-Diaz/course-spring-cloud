a643185f52a0fcbd0649a04610ed6e80930d8d2f
Diego Jimenez <diegourieljd@gmail.com>
2023-04-11 17:35:18 -0600

    global filter updated

--- gateway-server/src/main/java/com/djimenez/gateway/filters/ExampleGlobalFilter.java
+++ gateway-server/src/main/java/com/djimenez/gateway/filters/ExampleGlobalFilter.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
@@ -34,14 +33,14 @@ public class ExampleGlobalFilter implements GlobalFilter, Ordered{
 			});
 			
 			exchange.getResponse().getCookies().add("color", ResponseCookie.from("color", "red").build());
-			exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
+			//exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
 		}));
 	}
 
 
 	@Override
 	public int getOrder() {
-		return -1;
+		return 1;
 	}
 
 }
