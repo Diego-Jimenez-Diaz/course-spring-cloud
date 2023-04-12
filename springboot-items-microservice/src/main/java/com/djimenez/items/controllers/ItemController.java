e64923d128cfa49041036073d45192f728151050
Diego Jimenez <diegourieljd@gmail.com>
2023-04-12 16:07:09 -0600

    personalized filters added

--- springboot-items-microservice/src/main/java/com/djimenez/items/controllers/ItemController.java
+++ springboot-items-microservice/src/main/java/com/djimenez/items/controllers/ItemController.java
@@ -5,12 +5,16 @@ import com.djimenez.items.models.Product;
 import com.djimenez.items.models.service.ItemService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
@@ -19,12 +23,17 @@ import java.util.List;
 @RestController
 public class ItemController {
 
+	
+	private static final Logger log = LoggerFactory.getLogger(ItemController.class);
+
     @Autowired
     @Qualifier("feignService")
     ItemService itemService;
 
     @GetMapping
-    public ResponseEntity<List<Item>> getItems(){
+    public ResponseEntity<List<Item>> getItems(@RequestParam(name="name", required = false) String name, @RequestHeader(name = "token-request", required = false) String token){
+    	log.info(String.format("name: %s", name));
+    	log.info(String.format("Token: %s", token));
         return new ResponseEntity<List<Item>>(itemService.getAll(), HttpStatus.OK);
     }
 
