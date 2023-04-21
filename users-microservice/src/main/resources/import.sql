cc60ab76634171d719b47de850f52e3dba13e4df
Diego Jimenez <diegourieljd@gmail.com>
2023-04-21 17:41:09 -0600

    password changed

--- users-microservice/src/main/resources/import.sql
+++ users-microservice/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO users (username, password, enabled, first_name, last_name,email) VALUES ('diegojd', '1234', 1, 'Diego', 'Jimenez', 'diego@example.com');
-INSERT INTO users (username, password, enabled, first_name, last_name,email) VALUES ('jimenezd', '0000', 1, 'D', 'J', 'jd@example.com');
+INSERT INTO users (username, password, enabled, first_name, last_name,email) VALUES ('diegojd', '$2a$10$mKea2JVrOIezdscpQy58QeJZoBbD56eW0d5P55/5VY.NooIhwKSWm', 1, 'Diego', 'Jimenez', 'diego@example.com');
+INSERT INTO users (username, password, enabled, first_name, last_name,email) VALUES ('jimenezd', '$2a$10$UThLfJA24ufUPLWQXFSplO8WauvoIAF8W9NBG6IllwQDyOWzWByIu', 1, 'D', 'J', 'jd@example.com');
 
 INSERT INTO roles (name) VALUES ('ROLE_USER');
 INSERT INTO roles (name) VALUES ('ROLE_ADMIN');
